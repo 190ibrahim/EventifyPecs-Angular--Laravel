792ec59b3328593b5b81e4ec9084a5fa98a31eed
190ibrahim <190ibrahimahmed@gmail.com>
2023-06-16 03:10:10 +0200

    Project Outline DONE

--- Front_end/src/app/dashboard/dashboard.component.ts
+++ Front_end/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  innerWidth: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
   }
 
+  @HostListener('window:resize', ['$event'])
+    onResize(event: any) {
+      this.innerWidth = window.innerWidth;
+  }
+  getClass() {
+    return this.innerWidth < 925? ' row-md' : 'row';
+  }
 }
+
+
+
+
+
+
