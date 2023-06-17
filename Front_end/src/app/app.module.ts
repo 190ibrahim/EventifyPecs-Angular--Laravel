0a66df99e6c1bd70fa874c741280876159dc03bb
Ussayed Shakeel <TTS72I@tr.pte.hu>
2023-06-17 17:31:01 +0200

    mine

--- Front_end/src/app/app.module.ts
+++ Front_end/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnnouncementComponent } from './components/announcement/announcement.component';
+import { CommentComponent } from './components/comment/comment.component';
+import { EventComponent } from './components/event/event.component';
+import { ListingComponent } from './components/listing/listing.component';
+import { ProductComponent } from './components/product/product.component';
+import { ServiceComponent } from './components/service/service.component';
+import { UserComponent } from './components/user/user.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnnouncementComponent,
+    CommentComponent,
+    EventComponent,
+    ListingComponent,
+    ProductComponent,
+    ServiceComponent,
+    UserComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
