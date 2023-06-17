78d99fed691881415061b8011af7de41616bcca1
Ussayed Shakeel <TTS72I@tr.pte.hu>
2023-06-17 17:31:23 +0200

    VDV

--- Front_end/src/app/app.module.ts
+++ Front_end/src/app/app.module.ts
@@ -1,34 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AnnouncementComponent } from './components/announcement/announcement.component';
-import { CommentComponent } from './components/comment/comment.component';
-import { EventComponent } from './components/event/event.component';
-import { ListingComponent } from './components/listing/listing.component';
-import { ProductComponent } from './components/product/product.component';
-import { ServiceComponent } from './components/service/service.component';
-import { UserComponent } from './components/user/user.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnnouncementComponent,
-    CommentComponent,
-    EventComponent,
-    ListingComponent,
-    ProductComponent,
-    ServiceComponent,
-    UserComponent,
-    RegisterComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
