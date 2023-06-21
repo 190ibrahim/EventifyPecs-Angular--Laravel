7804f6361c75ddccd5cddece79573a120d698d49
Ussayed Shakeel <TTS72I@tr.pte.hu>
2023-06-21 23:08:14 +0200

    event update

--- Back_end/app/Http/Controllers/EventsController.php
+++ Back_end/app/Http/Controllers/EventsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use Illuminate\Http\Request;
+use Validator;
 use Illuminate\Support\Facades\DB;
   class EventsController extends Controller
 {
@@ -28,28 +29,26 @@ use Illuminate\Support\Facades\DB;
 
     public function registerEvent(Request $request){
 
-        $event = $request->only('id', 'event_title', 'event_description', 'start_date', 'end_date', 'user_id');
-
-        // Fetch the email from the `users` table based on the user_id using a join
-        $email = DB::table('reg_events')
-            ->join('users', 'reg_events.user_id', '=', 'users.id')
-            ->where('reg_events.user_id', $event['user_id'])
-            ->value('users.email');
-
-        // Add the fetched email to the event data
-        $event['email'] = $email;
-
+        $validator = Validator::make($request->all(), [
+        'event_title' => 'required',
+        'event_description' => 'required',
+        'start_date' => 'required',
+        'end_date' => 'required',
+        'user_id' => 'required',
+        
+        ]);
+        
+        if ($validator->fails()) {
+        return response()->json($validator->errors()->toJson(), 400);
+        }
+        
+        
         // Insert the event data into the `reg_events` table using DB
-        DB::table('reg_events')->insert($event);
-
-        return response()->json(['message' => 'Event registered successfully'], 201);
-        // $event = $request->only('id', 'event_title', 'event_description', 'start_date', 'end_date', 'email');
-
-        // // Insert the event data into the `reg_events` table using DB
-        // DB::table('reg_events')->insert($event);
-
-        // return response()->json(['message' => 'Event registered successfully'], 201);
-    }
+        DB::table('reg_events')->insert($request->all());
+        
+        return response()->json(['message' => 'Event registered successfully'], 200);
+        
+        }
     public function store(Request $request)
     {
         $data = $request->validate([
