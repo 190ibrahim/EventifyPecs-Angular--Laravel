1a258379128215d95ed82a82114ec53053e90995
190ibrahim <190ibrahimahmed@gmail.com>
2023-06-23 01:20:06 +0200

    Events list and event calender dynamic dta partialy DONE

--- Front_end/src/app/events/event-list/event-list.component.ts
+++ Front_end/src/app/events/event-list/event-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
   selector: 'app-event-list',
@@ -6,8 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./event-list.component.css']
 })
 export class EventListComponent {
-url: string = "../assets/img1.jpg";
-    imageChange(event: any){
-        this.url = event.target.src;
-    }
+
+  url: string = "../assets/img1.jpg";
+
+  public events: any = [];
+  public isAdmin: any;
+
+
+
+  constructor( private auth: AuthService, private router: Router, ) {}
+
+
+  imageChange(event: any){
+      this.url = event.target.src;
+  }
+
+  ngOnInit(): void {
+    this.getEvents();
+    this.isAdmin = localStorage.getItem('admin');
+  }
+
+  getEvents() {
+    this.auth.getEvents().subscribe(
+      (res: any) => {
+        this.events = res;
+        console.log(this.events);
+      },
+      (err: any) => {}
+    );
+  }
 }
